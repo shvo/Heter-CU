366c1e09101fbc901e2c119f88990e375a85eb74
pku-pc-shuowang <shvowang@gmail.com>
2016-09-07 11:25:12 +0800

    jacobi2D_fpga_heter-ghost.c is added for heter-ghost

---
+++ opencl/jacobi2D/jacobi2D_fpga_heter-ghost.c
@@ -0,0 +1,528 @@
+/**
+ * jacobi2D.c: This file is part of the PolyBench/GPU 1.0 test suite.
+ *
+ *
+ * Contact: Scott Grauer-Gray <sgrauerg@gmail.com>
+ * Will Killian <killian@udel.edu>
+ * Louis-Noel Pouchet <pouchet@cse.ohio-state.edu>
+ * Web address: http://www.cse.ohio-state.edu/~pouchet/software/polybench/GPU
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/time.h>
+
+#define POLYBENCH_TIME 1
+
+#define XX 32
+#define YY 32
+#define TT 1
+
+//select the OpenCL device to use (can be GPU, CPU, or Accelerator such as Intel Xeon Phi)
+//#define OPENCL_DEVICE_SELECTION CL_DEVICE_TYPE_CPU
+//#define OPENCL_DEVICE_SELECTION CL_DEVICE_TYPE_GPU
+#define OPENCL_DEVICE_SELECTION CL_DEVICE_TYPE_ACCELERATOR
+
+#include "jacobi2D.h"
+#include <polybench.h>
+#include <polybenchUtilFuncts.h>
+
+//define the error threshold for the results "not matching"
+#define PERCENT_DIFF_ERROR_THRESHOLD 0.05
+
+#ifdef __APPLE__
+#include <OpenCL/opencl.h>
+#else
+#include <CL/cl.h>
+#endif
+
+#define MAX_SOURCE_SIZE (0x100000)
+
+#if defined(cl_khr_fp64)  // Khronos extension available?
+#pragma OPENCL EXTENSION cl_khr_fp64 : enable
+#elif defined(cl_amd_fp64)  // AMD extension available?
+#pragma OPENCL EXTENSION cl_amd_fp64 : enable
+#endif
+
+char str_temp[1024];
+
+cl_platform_id platform_id;
+cl_platform_id *platforms;
+cl_device_id device_id;   
+cl_uint num_devices;
+cl_uint num_platforms;
+cl_int errcode;
+cl_context clGPUContext;
+cl_kernel clKernel1;
+cl_kernel clKernel2;
+cl_kernel clKernel3;
+cl_kernel clKernel4;
+cl_kernel clConnect_1_4;
+cl_command_queue clCommandQue;
+cl_program clProgram;
+cl_mem a_mem_obj;
+cl_mem b_mem_obj;
+cl_mem c_mem_obj;
+FILE *fp;
+char *source_str;
+size_t source_size;
+
+#define RUN_ON_CPU
+
+int
+load_file_to_memory(const char *filename, char **result)
+{ 
+  int size = 0;
+  FILE *f = fopen(filename, "rb");
+  if (f == NULL) 
+  { 
+    *result = NULL;
+    return -1; // -1 means file opening fail 
+  } 
+  fseek(f, 0, SEEK_END);
+  size = ftell(f);
+  fseek(f, 0, SEEK_SET);
+  *result = (char *)malloc(size+1);
+  if (size != fread(*result, sizeof(char), size, f)) 
+  { 
+    free(*result);
+    return -2; // -2 means file reading fail 
+  } 
+  fclose(f);
+  (*result)[size] = 0;
+  return size;
+}
+
+
+void compareResults(int n, DATA_TYPE POLYBENCH_2D(a,N,N,n,n), DATA_TYPE POLYBENCH_2D(a_outputFromGpu,N,N,n,n), DATA_TYPE POLYBENCH_2D(b,N,N,n,n), DATA_TYPE POLYBENCH_2D(b_outputFromGpu,N,N,n,n))
+{
+	int i, j, fail;
+	fail = 0;   
+
+	// Compare output from CPU and GPU
+	for (i=0; i<n; i++) 
+	{
+		for (j=0; j<n; j++) 
+		{
+			if (percentDiff(a[i][j], a_outputFromGpu[i][j]) > PERCENT_DIFF_ERROR_THRESHOLD) 
+			{
+				fail++;
+			}
+                }
+	}
+  
+        /*
+	for (i=0; i<n; i++) 
+	{
+       	        for (j=0; j<n; j++) 
+		{
+        		if (percentDiff(b[i][j], b_outputFromGpu[i][j]) > PERCENT_DIFF_ERROR_THRESHOLD) 
+			{
+        			fail++;
+        		}
+       	        }
+	}
+        */
+
+	// Print results
+	printf("Non-Matching CPU-GPU Outputs Beyond Error Threshold of %4.2f Percent: %d\n", PERCENT_DIFF_ERROR_THRESHOLD, fail);
+}
+
+
+#if OPENCL_DEVICE_SELECTION!=CL_DEVICE_TYPE_ACCELERATOR
+void read_cl_file()
+#else
+void read_cl_file(char** argv)
+#endif
+{
+        #if OPENCL_DEVICE_SELECTION!=CL_DEVICE_TYPE_ACCELERATOR
+	// Load the kernel source code into the array source_str
+	fp = fopen("jacobi2D_gpu_ghost.cl", "r");
+	if (!fp) {
+		fprintf(stderr, "Failed to load kernel.\n");
+		exit(1);
+	}
+	source_str = (char*)malloc(MAX_SOURCE_SIZE);
+	source_size = fread( source_str, 1, MAX_SOURCE_SIZE, fp);
+	fclose( fp );
+        #else 
+        printf("loading %s\n", argv[1]);
+        source_size = load_file_to_memory(argv[1], (char **) &source_str);
+        if (source_size < 0) {
+          printf("failed to load kernel from xclbin: %s\n", argv[1]);
+        }
+        #endif
+
+}
+
+
+void init_array(int n, DATA_TYPE POLYBENCH_2D(A,N,N,n,n), DATA_TYPE POLYBENCH_2D(B,N,N,n,n))
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			A[i][j] = ((DATA_TYPE) i*(j+2) + 10) / N;
+			B[i][j] = ((DATA_TYPE) (i-4)*(j-1) + 11) / N;
+		}
+	}
+}
+
+
+void cl_initialization()
+{
+	// Get platform and device information
+	errcode = clGetPlatformIDs(1, &platform_id, &num_platforms);
+	if(errcode == CL_SUCCESS) printf("number of platforms is %d\n",num_platforms);
+        // get all platforms
+        platforms = (cl_platform_id*) malloc(sizeof(cl_platform_id) * num_platforms);
+        errcode = clGetPlatformIDs(num_platforms, platforms, NULL);
+        if (errcode != CL_SUCCESS)
+        {
+         printf("Error: Failed to get PlatformIDs!\n");
+        }
+
+        // GPU platform id
+        #if OPENCL_DEVICE_SELECTION==CL_DEVICE_TYPE_CPU
+        platform_id = platforms[1];
+        #elif OPENCL_DEVICE_SELECTION==CL_DEVICE_TYPE_GPU
+        platform_id = platforms[0];
+        #else 
+        platform_id = platforms[2];
+        #endif
+
+
+	errcode = clGetPlatformInfo(platform_id,CL_PLATFORM_NAME, sizeof(str_temp), str_temp,NULL);
+	if(errcode == CL_SUCCESS) printf("platform name is %s\n",str_temp);
+
+	errcode = clGetPlatformInfo(platform_id, CL_PLATFORM_VERSION, sizeof(str_temp), str_temp,NULL);
+	if(errcode == CL_SUCCESS) printf("platform version is %s\n",str_temp);
+
+	errcode = clGetDeviceIDs( platform_id, OPENCL_DEVICE_SELECTION, 1, &device_id, &num_devices);
+	if(errcode == CL_SUCCESS) printf("device id is %d\n",device_id);
+
+	errcode = clGetDeviceInfo(device_id,CL_DEVICE_NAME, sizeof(str_temp), str_temp,NULL);
+	if(errcode == CL_SUCCESS) printf("device name is %s\n",str_temp);
+	
+	// Create an OpenCL context
+	clGPUContext = clCreateContext( NULL, 1, &device_id, NULL, NULL, &errcode);
+	if(errcode != CL_SUCCESS) printf("Error in creating context\n");
+ 
+	//Create a command-queue
+	clCommandQue = clCreateCommandQueue(clGPUContext, device_id, CL_QUEUE_OUT_OF_ORDER_EXEC_MODE_ENABLE, &errcode);
+	if(errcode != CL_SUCCESS) printf("Error in creating command queue\n");
+}
+
+
+void cl_mem_init(DATA_TYPE POLYBENCH_2D(A,N,N,n,n), DATA_TYPE POLYBENCH_2D(B,N,N,n,n))
+{
+	a_mem_obj = clCreateBuffer(clGPUContext, CL_MEM_READ_WRITE, N * N * sizeof(DATA_TYPE), NULL, &errcode);
+	b_mem_obj = clCreateBuffer(clGPUContext, CL_MEM_READ_WRITE, N * N * sizeof(DATA_TYPE), NULL, &errcode);
+		
+	if(errcode != CL_SUCCESS) printf("Error in creating buffers\n");
+
+	errcode = clEnqueueWriteBuffer(clCommandQue, a_mem_obj, CL_TRUE, 0, N * N * sizeof(DATA_TYPE), A, 0, NULL, NULL);
+	errcode = clEnqueueWriteBuffer(clCommandQue, b_mem_obj, CL_TRUE, 0, N * N * sizeof(DATA_TYPE), B, 0, NULL, NULL);
+	if(errcode != CL_SUCCESS)printf("Error in writing buffers\n");
+}
+
+
+void cl_load_prog()
+{
+        #if OPENCL_DEVICE_SELECTION!=CL_DEVICE_TYPE_ACCELERATOR
+	// Create a program from the kernel source
+	clProgram = clCreateProgramWithSource(clGPUContext, 1, (const char **)&source_str, (const size_t *)&source_size, &errcode);
+        #else
+        // Create a program from offline binary
+        int status;
+        clProgram = clCreateProgramWithBinary(clGPUContext, 1, &device_id, &source_size, (const unsigned char **) &source_str, &status, &errcode);
+        #endif
+
+	if(errcode != CL_SUCCESS) printf("Error in creating program\n");
+
+        #if OPENCL_DEVICE_SELECTION!=CL_DEVICE_TYPE_ACCELERATOR
+	// Build the program
+	errcode = clBuildProgram(clProgram, 1, &device_id, NULL, NULL, NULL);
+        #else
+        errcode = clBuildProgram(clProgram, 0, NULL, NULL, NULL, NULL);
+        #endif
+	if(errcode != CL_SUCCESS) {
+            size_t len;
+            char buffer[2048];
+
+            printf("Error %d in building program\n", errcode);
+            clGetProgramBuildInfo(clProgram, device_id, CL_PROGRAM_BUILD_LOG, sizeof(buffer), buffer, &len);
+            printf("%s\n", buffer);
+        }
+		
+	// Create the OpenCL kernel
+	clKernel1 = clCreateKernel(clProgram, "runJacobi2D_kernel1", &errcode);
+	if(errcode != CL_SUCCESS) printf("Error in creating kernel1\n");
+	clKernel2 = clCreateKernel(clProgram, "runJacobi2D_kernel2", &errcode);
+	if(errcode != CL_SUCCESS) printf("Error in creating kernel2\n");
+        clKernel3 = clCreateKernel(clProgram, "runJacobi2D_kernel3", &errcode);
+	if(errcode != CL_SUCCESS) printf("Error in creating kernel3\n");
+	clKernel4 = clCreateKernel(clProgram, "runJacobi2D_kernel4", &errcode);
+	if(errcode != CL_SUCCESS) printf("Error in creating kernel4\n");
+	clConnect_1_4 = clCreateKernel(clProgram, "runJacobi1D_connect_1_4", &errcode);
+	if(errcode != CL_SUCCESS) printf("Error in creating connect_1_4 kernel\n");
+	clFinish(clCommandQue);
+}
+
+void cl_launch_kernel1(int n)
+{
+	size_t localWorkSize[2], globalWorkSize[2];
+	//localWorkSize[0] = DIM_LOCAL_WORK_GROUP_X;
+	//localWorkSize[1] = DIM_LOCAL_WORK_GROUP_Y;
+        localWorkSize[0] = 1;
+	localWorkSize[1] = 1;
+	globalWorkSize[0] = N / XX;
+	globalWorkSize[1] = N / YY;
+	
+	// Set the arguments of the kernel
+	errcode =  clSetKernelArg(clKernel1, 0, sizeof(cl_mem), (void *)&a_mem_obj);
+	errcode |= clSetKernelArg(clKernel1, 1, sizeof(cl_mem), (void *)&b_mem_obj);
+	errcode |= clSetKernelArg(clKernel1, 2, sizeof(int), (void *)&n);
+	if(errcode != CL_SUCCESS) printf("Error in seting arguments of kernel1\n");
+
+	errcode =  clSetKernelArg(clKernel2, 0, sizeof(cl_mem), (void *)&a_mem_obj);
+	errcode |= clSetKernelArg(clKernel2, 1, sizeof(cl_mem), (void *)&b_mem_obj);
+	errcode |= clSetKernelArg(clKernel2, 2, sizeof(int), (void *)&n);
+	if(errcode != CL_SUCCESS) printf("Error in seting arguments of kernel2\n");
+
+	errcode =  clSetKernelArg(clKernel3, 0, sizeof(cl_mem), (void *)&a_mem_obj);
+	errcode |= clSetKernelArg(clKernel3, 1, sizeof(cl_mem), (void *)&b_mem_obj);
+	errcode |= clSetKernelArg(clKernel3, 2, sizeof(int), (void *)&n);
+	if(errcode != CL_SUCCESS) printf("Error in seting arguments of kernel3\n");
+
+	errcode =  clSetKernelArg(clKernel4, 0, sizeof(cl_mem), (void *)&a_mem_obj);
+	errcode |= clSetKernelArg(clKernel4, 1, sizeof(cl_mem), (void *)&b_mem_obj);
+	errcode |= clSetKernelArg(clKernel4, 2, sizeof(int), (void *)&n);
+	if(errcode != CL_SUCCESS) printf("Error in seting arguments of kernel4\n");
+
+	errcode =  clSetKernelArg(clConnect_1_4, 0, sizeof(cl_mem), (void *)&a_mem_obj);
+	if(errcode != CL_SUCCESS) printf("Error in seting arguments of connect_1_4\n");
+
+	// Execute the OpenCL kernel
+        cl_event event_kernel1;
+	errcode = clEnqueueNDRangeKernel(clCommandQue, clKernel1, 2, NULL, globalWorkSize, localWorkSize, 0, NULL, &event_kernel1);
+	if(errcode != CL_SUCCESS) printf("Error in launching kernel1\n");
+        else printf("kernel1 is launched\n");
+
+        cl_event event_kernel2;
+	errcode = clEnqueueNDRangeKernel(clCommandQue, clKernel2, 2, NULL, globalWorkSize, localWorkSize, 0, NULL, &event_kernel2);
+	if(errcode != CL_SUCCESS) printf("Error in launching kernel2\n");
+        else printf("kernel2 is launched\n");
+
+        cl_event event_kernel3;
+	errcode = clEnqueueNDRangeKernel(clCommandQue, clKernel3, 2, NULL, globalWorkSize, localWorkSize, 0, NULL, &event_kernel3);
+	if(errcode != CL_SUCCESS) printf("Error in launching kernel3\n");
+        else printf("kernel3 is launched\n");
+
+        cl_event event_kernel4;
+	errcode = clEnqueueNDRangeKernel(clCommandQue, clKernel4, 2, NULL, globalWorkSize, localWorkSize, 0, NULL, &event_kernel4);
+	if(errcode != CL_SUCCESS) printf("Error in launching kernel4\n");
+        else printf("kernel4 is launched\n");
+
+        cl_event event_connect_1_4;
+	errcode = clEnqueueNDRangeKernel(clCommandQue, clConnect_1_4, 2, NULL, globalWorkSize, localWorkSize, 0, NULL, &event_connect_1_4);
+	if(errcode != CL_SUCCESS) printf("Error in launching connect_1_4\n");
+        else printf("connect_1_4 is launched\n");
+
+	clFinish(clCommandQue);
+}
+
+void cl_launch_kernel2(int n)
+{
+	size_t localWorkSize[2], globalWorkSize[2];
+	localWorkSize[0] = DIM_LOCAL_WORK_GROUP_X;
+	localWorkSize[1] = DIM_LOCAL_WORK_GROUP_Y;
+	globalWorkSize[0] = N;
+	globalWorkSize[1] = N;
+	
+	// Set the arguments of the kernel
+	errcode =  clSetKernelArg(clKernel2, 0, sizeof(cl_mem), (void *)&a_mem_obj);
+	errcode |= clSetKernelArg(clKernel2, 1, sizeof(cl_mem), (void *)&b_mem_obj);
+	errcode |= clSetKernelArg(clKernel2, 2, sizeof(int), (void *)&n);
+	if(errcode != CL_SUCCESS) printf("Error in seting arguments\n");
+
+	// Execute the OpenCL kernel
+	errcode = clEnqueueNDRangeKernel(clCommandQue, clKernel2, 2, NULL, globalWorkSize, localWorkSize, 0, NULL, NULL);
+	if(errcode != CL_SUCCESS) printf("Error in launching kernel\n");
+	clFinish(clCommandQue);
+}
+
+void cl_launch_kernels()
+{
+	size_t localWorkSize[2], globalWorkSize[2];
+	localWorkSize[0] = DIM_LOCAL_WORK_GROUP_X;
+	localWorkSize[1] = DIM_LOCAL_WORK_GROUP_Y;
+	globalWorkSize[0] = N;
+	globalWorkSize[1] = N;
+	int t;
+	
+	for (t = 0; t < TSTEPS ; t++)
+	{	
+		// Execute the OpenCL kernel
+		errcode = clEnqueueNDRangeKernel(clCommandQue, clKernel1, 2, NULL, globalWorkSize, localWorkSize, 0, NULL, NULL);
+		if(errcode != CL_SUCCESS) printf("Error in launching kernel\n");
+		clFinish(clCommandQue);
+
+		// Execute the OpenCL kernel
+		//errcode = clEnqueueNDRangeKernel(clCommandQue, clKernel2, 2, NULL, globalWorkSize, localWorkSize, 0, NULL, NULL);
+		//if(errcode != CL_SUCCESS) printf("Error in launching kernel\n");
+		//clFinish(clCommandQue);
+	}
+}
+
+void cl_clean_up()
+{
+	// Clean up
+	errcode = clFlush(clCommandQue);
+	errcode = clFinish(clCommandQue);
+	errcode = clReleaseKernel(clKernel1);
+	errcode = clReleaseKernel(clKernel2);
+	errcode = clReleaseKernel(clKernel3);
+	errcode = clReleaseKernel(clKernel4);
+	errcode = clReleaseKernel(clConnect_1_4);
+	errcode = clReleaseProgram(clProgram);
+	errcode = clReleaseMemObject(a_mem_obj);
+	errcode = clReleaseMemObject(b_mem_obj);
+	errcode = clReleaseCommandQueue(clCommandQue);
+	errcode = clReleaseContext(clGPUContext);
+	if(errcode != CL_SUCCESS) printf("Error in cleanup\n");
+}
+
+
+void runJacobi2DCpu(int tsteps, int n, DATA_TYPE POLYBENCH_2D(A,N,N,n,n), DATA_TYPE POLYBENCH_2D(B,N,N,n,n))
+{
+	int t, i, j;
+	for (t = 0; t < _PB_TSTEPS; t++)
+	{
+    		for (i = 1; i < _PB_N - 1; i++)
+		{
+			for (j = 1; j < _PB_N - 1; j++)
+			{
+	  			B[i][j] = 0.2f * (A[i][j] + A[i][(j-1)] + A[i][(1+j)] + A[(1+i)][j] + A[(i-1)][j]);
+			}
+		}
+		
+    		for (i = 1; i < _PB_N-1; i++)
+		{
+			for (j = 1; j < _PB_N-1; j++)
+			{
+	  			A[i][j] = B[i][j];
+			}
+		}
+	}
+}
+
+
+/* DCE code. Must scan the entire live-out data.
+   Can be used also to check the correctness of the output. */
+static
+void print_array(int n,
+		 DATA_TYPE POLYBENCH_2D(A,N,N,n,n))
+
+{
+  int i, j;
+
+  for (i = 0; i < n; i++)
+    for (j = 0; j < n; j++) {
+      fprintf(stderr, DATA_PRINTF_MODIFIER, A[i][j]);
+      if ((i * n + j) % 20 == 0) fprintf(stderr, "\n");
+    }
+  fprintf(stderr, "\n");
+}
+
+
+int main(int argc, char *argv[])
+{
+	/* Retrieve problem size. */
+	int n = N;
+	int tsteps = TSTEPS;
+
+	POLYBENCH_2D_ARRAY_DECL(a,DATA_TYPE,N,N,n,n);
+	POLYBENCH_2D_ARRAY_DECL(b,DATA_TYPE,N,N,n,n);
+	POLYBENCH_2D_ARRAY_DECL(a_outputFromGpu,DATA_TYPE,N,N,n,n);
+	POLYBENCH_2D_ARRAY_DECL(b_outputFromGpu,DATA_TYPE,N,N,n,n);
+
+	init_array(n, POLYBENCH_ARRAY(a), POLYBENCH_ARRAY(b));
+
+        #if OPENCL_DEVICE_SELECTION!=CL_DEVICE_TYPE_ACCELERATOR
+	read_cl_file();
+        #else
+        if (argc != 2){
+          printf("%s <inputfile>\n", argv[0]);
+          return EXIT_FAILURE;
+        }
+        read_cl_file(argv);
+        #endif
+
+	cl_initialization();
+	cl_mem_init(POLYBENCH_ARRAY(a), POLYBENCH_ARRAY(b));
+	cl_load_prog();
+	
+	/* Start timer. */
+  	polybench_start_instruments;
+
+	int t;
+	for (t = 0; t < _PB_TSTEPS/TT; t++)
+    	{
+                printf("t = %d\n", t);
+		cl_launch_kernel1(n);
+		//cl_launch_kernel2(n);
+	}
+
+
+	/* Stop and print timer. */
+        #if OPENCL_DEVICE_SELECTION==CL_DEVICE_TYPE_CPU
+        printf("OpenCL-CPU Time in seconds: ");
+        #elif OPENCL_DEVICE_SELECTION==CL_DEVICE_TYPE_GPU
+        printf("OpenCL-GPU Time in seconds: ");
+        #else 
+        printf("OpenCL-FPGA Time in seconds: ");
+        #endif
+  	polybench_stop_instruments;
+ 	polybench_print_instruments;
+	
+	errcode = clEnqueueReadBuffer(clCommandQue, a_mem_obj, CL_TRUE, 0, N * N * sizeof(DATA_TYPE), POLYBENCH_ARRAY(a_outputFromGpu), 0, NULL, NULL);
+	errcode = clEnqueueReadBuffer(clCommandQue, b_mem_obj, CL_TRUE, 0, N * N * sizeof(DATA_TYPE), POLYBENCH_ARRAY(b_outputFromGpu), 0, NULL, NULL);
+	
+	if(errcode != CL_SUCCESS) printf("Error in reading GPU mem\n");
+
+
+	#ifdef RUN_ON_CPU
+		
+		/* Start timer. */
+	  	polybench_start_instruments;
+
+		runJacobi2DCpu(tsteps, n, POLYBENCH_ARRAY(a), POLYBENCH_ARRAY(b));
+	
+		/* Stop and print timer. */
+		printf("CPU Time in seconds:\n");
+	  	polybench_stop_instruments;
+	 	polybench_print_instruments;
+
+		compareResults(n, POLYBENCH_ARRAY(a), POLYBENCH_ARRAY(a_outputFromGpu), POLYBENCH_ARRAY(b), POLYBENCH_ARRAY(b_outputFromGpu));
+
+	#else //prevent dead code elimination
+
+		polybench_prevent_dce(print_array(n, POLYBENCH_ARRAY(a_outputFromGpu)));
+
+	#endif //RUN_ON_CPU
+
+
+	cl_clean_up();
+	POLYBENCH_FREE_ARRAY(a);
+	POLYBENCH_FREE_ARRAY(a_outputFromGpu);
+	POLYBENCH_FREE_ARRAY(b);
+	POLYBENCH_FREE_ARRAY(b_outputFromGpu);
+    
+	return 0;
+}
+
+#include <polybench.c>
